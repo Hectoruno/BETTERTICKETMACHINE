d50d6afcbb5005d3cc47e180af1eeaacd657aac9
Héctor García García <Hectoruno>
2017-10-27 12:26:01 +0200

    Ahora creamos una manera de que al crear una máquina podamos elegir si es una máquina normal o premiada, al optar por la premiada cada vez que compremos un billete recibiremos un billete adicional de regalo

--- TicketMachine.java
+++ TicketMachine.java
@@ -17,15 +17,18 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
+    // Máquina con premio
+    private boolean maquinaPremiada;
 
     /**
      * Create a machine that issues tickets of the given price.
      */
-    public TicketMachine(int cost)
+    public TicketMachine(int cost, boolean maquinaPremio)
     {
-        price = cost;
-        balance = 0;
-        total = 0;
+            price = cost;
+            balance = 0;
+            total = 0;
+            maquinaPremiada = maquinaPremio;
     }
 
     /**
@@ -66,7 +69,34 @@ public class TicketMachine
      */
     public void printTicket()
     {
-        if(balance >= price) {
+        if(balance >= price & maquinaPremiada == true) {
+            // Simulate the printing of a ticket.
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket");
+            System.out.println("# " + price + " cents.");
+            System.out.println("##################");
+            System.out.println();
+
+            // Update the total collected with the price.
+            total = total + price;
+            // Reduce the balance by the prince.
+            balance = balance - price;
+            
+            // Simulate the printing of a ticket.
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Billete de regalo");
+            System.out.println("# " + price + " cents.");
+            System.out.println("##################");
+            System.out.println();
+
+            // Update the total collected with the price.
+            total = total + price;
+            // Reduce the balance by the prince.
+            balance = balance - price; 
+        }
+        else if(balance >= price & maquinaPremiada == false) {
             // Simulate the printing of a ticket.
             System.out.println("##################");
             System.out.println("# The BlueJ Line");
@@ -100,21 +130,17 @@ public class TicketMachine
     }
 
     /**
-     * Vacía la maquina en caso de que no esté operando.
+     * Vacía la máquina en caso de que no esté operando.
      */
     public int emptyMachine()
     {
-        int status;
-        status = 0;
+        int status = -1;
         if(balance == 0) {
-            int vueltaDinero;
-            vueltaDinero = balance + total;
+            status = total;
             total = 0;
-            status = vueltaDinero;
         }
         else {
             System.out.println("Operación iniciada");
-            status = -1;
         }
         return status;
     }
